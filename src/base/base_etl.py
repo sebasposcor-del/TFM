b5b741f6eb173cc27f483be6cb37d94ac7398d18
Sebastian Posada Cortes <sebas.poscor@gmail.com>
2026-03-31 15:04:04 +0200

    black y isort

--- src/base/base_etl.py
+++ src/base/base_etl.py
@@ -1,40 +1,44 @@
-from abc import ABC, abstractmethod
-from email import utils
-import polars as pl
-from pymongo import MongoClient
 import logging
 import sys
+from abc import ABC, abstractmethod
+from email import utils
+
+import polars as pl
+from pymongo import MongoClient
+
 from utils.logger import get_logger
 
-
-#TYPE HINT
-#def transform(self, df: pl.DataFrame) -> pl.DataFrame:
+# TYPE HINT
+# def transform(self, df: pl.DataFrame) -> pl.DataFrame:
 #                            ↑ entra            ↑ sale
 
 
 class BaseETL(ABC):
-    """Clase base para procesos ETL, 
+    """Clase base para procesos ETL,
     define la estructura y métodos comunes para los ingesters"""
 
-    def __init__(self, mongo_uri:str = "mongodb://mongo:27017/", db_name:str = "tfm_energy"):
+    def __init__(
+        self, mongo_uri: str = "mongodb://mongo:27017/", db_name: str = "tfm_energy"
+    ):
         """Constructor de clase, es el metodo qeu se ejecuta automáticamente
         cuando se llama una clase que hereda este ETL base
            - self es la referencia al propio objeto"""
-        self.client = MongoClient(mongo_uri) 
+        self.client = MongoClient(mongo_uri)
         self.db = self.client[db_name]
-        self.logger = get_logger(self.__class__.__name__) #Crea un logger específico para cada clase que herede de BaseETL, usando el nombre de la clase como nombre del logger
+        self.logger = get_logger(
+            self.__class__.__name__
+        )  # Crea un logger específico para cada clase que herede de BaseETL, usando el nombre de la clase como nombre del logger
 
-    @abstractmethod #decoder que obliga a clases hijas a implementarlo
-
+    @abstractmethod  # decoder que obliga a clases hijas a implementarlo
     def extract(self) -> pl.DataFrame:
         """Método que descarga data de fuente,devuelve un dataFrame raw,"""
-        pass    
+        pass
 
     @abstractmethod
     def transform(self, df: pl.DataFrame) -> pl.DataFrame:
         """Método que limpia y transforma el dataFrame raw, devuelve un dataFrame limpio"""
         pass
- 
+
     @abstractmethod
     def load_raw(self, df: pl.DataFrame) -> None:
         """Método que carga el dataFrame raw a la base de datos"""
@@ -48,13 +52,13 @@ class BaseETL(ABC):
         pass
 
     def run(self) -> None:
-        """ Orquesta el pipeline ETL, llamando a los métodos en orden: extract, transform, load_raw, load_clean"""
+        """Orquesta el pipeline ETL, llamando a los métodos en orden: extract, transform, load_raw, load_clean"""
         self.logger.info("Iniciando  ETL...")
 
         raw_pl = self.extract()
         self.load_raw(raw_pl)
 
-        clean_pl  =self.transform(raw_pl)
+        clean_pl = self.transform(raw_pl)
         self.load_clean(clean_pl)
 
         self.client.close()
--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,18 +1,25 @@
 import logging
-import sys #Para que mpirma en la consola
+import sys  # Para que mpirma en la consola
 
-#Cadena logger -> handler -> formatter
-def get_logger(name:str) -> logging.Logger:
+
+# Cadena logger -> handler -> formatter
+def get_logger(name: str) -> logging.Logger:
     """Funcion para recivir nombre y devolver un objeto logger
     configurado. -> es solo el type hint del tipo de objeto que devuelve"""
-    logger = logging.getLogger(name) #Crea o recupera un logger con el nombre dado
-    logger.setLevel(logging.INFO) #Establece el nivel de logueo a INFO, lo que significa que se registrarán mensajes de nivel INFO y superiores (WARNING, ERROR, CRITICAL)
+    logger = logging.getLogger(name)  # Crea o recupera un logger con el nombre dado
+    logger.setLevel(
+        logging.INFO
+    )  # Establece el nivel de logueo a INFO, lo que significa que se registrarán mensajes de nivel INFO y superiores (WARNING, ERROR, CRITICAL)
 
-#Handler define a donde van los mensajes
-    if not logger.hasHandlers(): #Verifica si el logger ya tiene handlers configurados para evitar agregar múltiples handlers al mismo logger
-        handler = logging.StreamHandler(sys.stdout) #Crea un handler que envía los mensajes de logueo a la salida estándar (consola)
-        formatter = logging.Formatter( #Define el formato de los mensajes de logueo, incluyendo la fecha y hora, el nombre del logger, el nivel de logueo y el mensaje en sí
-            '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    # Handler define a donde van los mensajes
+    if (
+        not logger.hasHandlers()
+    ):  # Verifica si el logger ya tiene handlers configurados para evitar agregar múltiples handlers al mismo logger
+        handler = logging.StreamHandler(
+            sys.stdout
+        )  # Crea un handler que envía los mensajes de logueo a la salida estándar (consola)
+        formatter = logging.Formatter(  # Define el formato de los mensajes de logueo, incluyendo la fecha y hora, el nombre del logger, el nivel de logueo y el mensaje en sí
+            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
         )
         handler.setFormatter(formatter)
         logger.addHandler(handler)
